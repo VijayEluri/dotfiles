37d1cc49aa7e5c118d8e2d4c63ee5c8a09bcf67b
phua <pehua01@gmail.com>
2010-01-20 21:46:27 -0600

    generate tag file

--- .emacs.d/functions.el
+++ .emacs.d/functions.el
@@ -15,3 +15,9 @@
 ;;  (goto-char (point-min))
 ;;  (indent-region start end)
 ;;  (delete-blank-lines))
+
+(defun generate-tag-file (directory)
+  "Generate tag file."
+  (interactive "DDirectory name: ")
+  ;;(shell-command (format "ctags -fs %s/TAGS -e -R %s" directory directory)
+  (eshell-command (format "find %s -type f -name \\*.java | xargs etags --language-force=Java" directory)))
