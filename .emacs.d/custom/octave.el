64e4bfda447fdca24f458fd5067d85ec7abd5545
Peter Hua <pehua01@gmail.com>
2011-01-20 01:09:32 -0600

    update

--- .emacs.d/custom/octave.el
+++
@@ -1,8 +0,0 @@
-(autoload 'run-octave "octave-inf" nil t)
-(add-hook 'inferior-octave-mode-hook
-	  (lambda ()
-	    (turn-on-font-lock)
-	    (define-key inferior-octave-mode-map [up]
-	      'comint-previous-input)
-	    (define-key inferior-octave-mode-map [down]
-	      'comint-next-input)))
