c99659436045855839ec4aaec721f13147432715
Peter Hua <pehua01@gmail.com>
2011-05-11 03:18:57 -0500

    emacs ess

---
+++ .emacs.d/custom/rrc.el
@@ -0,0 +1,11 @@
+(load (concat local-load-path "/ess/lisp/ess-site"))
+
+(require 'ess-site)
+
+;; (setq ess-ask-for-ess-directory nil
+;;       ess-directory "~/development/r/workspace")
+
+;; M-x S
+;;
+;; Inferior ESS
+;; M-x ?    ess-list-object-completions
