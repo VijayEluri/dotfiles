6607bbf9d066add26da7938c224e1591eaf42c64
Peter Hua <pehua01@gmail.com>
2011-12-29 02:35:41 -0600

    pymacs

--- .emacs.d/lisp/pyrc.el
+++ .emacs.d/lisp/pyrc.el
@@ -1,11 +1,7 @@
-(add-to-list 'load-path (concat lisp-directory "/python"))
-
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode) interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-
-(setq font-lock-maximum-decoration t)
-
+(add-to-list 'load-path (concat site-lisp-directory "/python"))
+(autoload 'python-mode "python-mode" "Python mode." t)
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (require 'python-mode)
 
 (autoload 'pymacs-apply "pymacs")
@@ -14,19 +10,16 @@
 (autoload 'pymacs-exec "pymacs" nil t)
 (autoload 'pymacs-load "pymacs" nil t)
 
-(eval-after-load "pymacs" '(add-to-list 'pymacs-load-path (concat lisp-directory "/pymacs")))
+(eval-after-load "pymacs"
+  '(add-to-list 'pymacs-load-path (concat lisp-directory "/pymacs")))
 
-;; M-x pymacs-eval repr(2**111)
-;; M-x pymacs-load os
-;; M-: (os-getcwd)
-
+(setq ropemacs-enable-shortcuts t)
+(setq ropemacs-local-prefix "C-c r")
+(setq ropemacs-global-prefix "C-c C-p")
+(setq ropemacs-enable-autoimport t)
+(setq ropemacs-autoimport-modules '("os", "shutil"))
 (require 'pymacs)
-(pymacs-load "ropemacs" "rope-" t)
-(setq ropemacs-confirm-saving nil)
-;; (setq ropemacs-enable-autoimport t)
-;; (setq ropemacs-autoimport-modules '("os", "shutil"))
-;; (setq ropemacs-enable-shortcuts nil)
-;; (setq ropemacs-local-prefix "C-c C-p")
+(pymacs-load "ropemacs" "rope-")
 
 (add-to-list 'load-path (concat global-load-path "/python-mode"))
 (setq ipython-command "/usr/bin/ipython")
