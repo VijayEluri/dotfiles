c722fb0a92c8e2a4101808bb93f37f7a73098128
Peter Hua <pehua01@gmail.com>
2014-10-04 22:15:52 -0500

    Cleanup

--- .emacs.d/lisp/rrc.el
+++
@@ -1,7 +0,0 @@
-(load (concat local-lisp-directory "/ess/lisp/ess-site"))
-
-(require 'ess-site)
-
-(setq ess-ask-for-ess-directory nil
-      ess-directory "~/development/r/workspace/"
-      ess-ask-about-transfile nil)
