4429d35a950087cf9ec1594a00145df9ec6189f4
Peter Hua <pehua01@gmail.com>
2014-10-04 23:10:49 -0500

    Cleanup

--- usr/local/bin/toggle-touchpad.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import subprocess
-
-result = subprocess.check_output("synclient -l | grep TouchpadOff | awk '{ print $3 }'", shell=True)
-if (result == b'0\n'):
-    subprocess.call("synclient TouchpadOff=1", shell=True)
-else:
-    subprocess.call("synclient TouchpadOff=0", shell=True)
