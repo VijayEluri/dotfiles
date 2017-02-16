3c50739f9be53ad2e1fab3dfd501417ee41ffe4b
Peter Hua <phua@users.noreply.github.com>
2017-02-16 02:07:20 -0600

    X

---
+++ X/.xmonad/xmonad.hs
@@ -0,0 +1,35 @@
+-- $ xmonad --recompile
+
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+myManageHook = composeAll [
+  className =? "stalonetray" --> doIgnore
+  ]
+
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+  -- dzproc <- spawnPipe "/usr/bin/dzen2"
+  xmonad $ defaultConfig {
+    manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig,
+    layoutHook = avoidStruts  $  layoutHook defaultConfig,
+    logHook    = dynamicLogWithPP $ xmobarPP {
+      ppOutput = hPutStrLn xmproc,
+      ppTitle  = xmobarColor "green" "" . shorten 50
+      },
+    terminal           = "urxvt",
+    focusFollowsMouse  = False,
+    modMask            = mod4Mask,
+    focusedBorderColor = "yellow",
+    startupHook = setWMName "LG3D" -- Java hack
+    } `additionalKeys`
+    [ ((mod4Mask, xK_x), spawn "/usr/bin/python ~/workspaces/python/touchpad/toggle-touchpad.py"), -- M-x
+      ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/xscreensaver-command -lock"), -- M-S-z
+      ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"), -- C-PrtSc
+      ((0, xK_Print), spawn "scrot")                          -- PrtSc
+    ]
